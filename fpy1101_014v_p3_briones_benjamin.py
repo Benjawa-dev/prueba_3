c9134c1351d88063934d8ed1237dd70da052152a
Benjawa-dev <be.brionesc@duocuc.cl>
2024-06-24 21:41:21 -0400

    Add files via upload

---
+++ fpy1101_014v_p3_briones_benjamin.py
@@ -0,0 +1,14 @@
+
+tipo = []
+patente = []
+marca = []
+
+while True:
+    opcion_menu = int(input("bienvenidos a Auto Seguro, ingrese los datos del vehiculo /n a) grabar, n/ b) buscar n/ c) imprimir certificados n/ d) salir"))
+    if opcion_menu == a:
+        tipo = int("ingrese el tipo de vehiculo")
+        patente = int("ingrese la patente(debe tener 4 letras constantes seguida por 2 numeros)")
+        marca = int("ingrese el nombre de la marca del vehiculo (debe contener mas de 2 a 15 caracteres)")
+        input(int("ingrese el nombre y apellido del usuario"))        
+        precio <5000000
+        
